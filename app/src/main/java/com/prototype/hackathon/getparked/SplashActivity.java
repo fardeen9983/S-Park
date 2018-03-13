ec90ba8829e3b280292a2d7888f617785e095e86
Fardeen Khan <khanfardeen9983@gmail.com>
2018-03-13 18:26:33 +0530

    Search bar added

--- app/src/main/java/com/prototype/hackathon/getparked/SplashActivity.java
+++ app/src/main/java/com/prototype/hackathon/getparked/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.prototype.hackathon.getparked;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        startActivity(new Intent(getApplicationContext(),MapActivity.class));
     }
 }
