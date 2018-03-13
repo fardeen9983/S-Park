4bc4c0872f8fcc95092124c6e761456be2710bf8
Fardeen Khan <khanfardeen9983@gmail.com>
2018-03-13 22:57:08 +0530

    sign up added

--- app/src/main/java/com/prototype/hackathon/getparked/AccountActivity.java
+++ app/src/main/java/com/prototype/hackathon/getparked/AccountActivity.java
@@ -1,13 +1,23 @@
 package com.prototype.hackathon.getparked;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AccountActivity extends AppCompatActivity {
 
     @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+        //return super.onSupportNavigateUp();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
